17b2da8bd0e4346df34a1e15133ab3f6bc247d56
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-03-07 12:34:45 -0500

    Drop all the fancy temp log business

--- src/ccstudiodss/cli.py
+++ src/ccstudiodss/cli.py
@@ -1,10 +1,5 @@
 from __future__ import unicode_literals
 
-import contextlib
-import os
-import shutil
-import tempfile
-
 import click
 import com.ti.ccstudio.scripting.environment
 
@@ -12,20 +7,6 @@ import com.ti.ccstudio.scripting.environment
 @click.group()
 def cli():
     pass
-
-
-@contextlib.contextmanager
-def file_or_temporary_file(f):
-    use_temp = f is None
-
-    if use_temp:
-        temporary_directory = tempfile.mkdtemp()
-        f = os.path.join(temporary_directory, 'dss_py.xml')
-
-    yield f
-
-    if use_temp:
-        shutil.rmtree(temporary_directory)
 
 
 @cli.command()
@@ -57,23 +38,22 @@ def load(ccxml, log, binary, trace_level):
 
     script.traceSetConsoleLevel(trace_level)
 
-    with file_or_temporary_file(log) as log:
+    if log is not None:
         script.traceBegin(log, 'DefaultStylesheet.xsl')
-
         script.traceSetFileLevel(trace_level)
 
-        script.setScriptTimeout(150000)
+    script.setScriptTimeout(150000)
 
-        debugServer = script.getServer('DebugServer.1')
-        debugServer.setConfig(ccxml)
+    debugServer = script.getServer('DebugServer.1')
+    debugServer.setConfig(ccxml)
 
-        debugSession = debugServer.openSession()
+    debugSession = debugServer.openSession()
 
-        debugSession.target.connect()
+    debugSession.target.connect()
 
-        debugSession.memory.loadProgram(binary)
+    debugSession.memory.loadProgram(binary)
 
-        debugSession.target.restart()
-        debugSession.target.runAsynch()
+    debugSession.target.restart()
+    debugSession.target.runAsynch()
 
-        debugServer.stop()
+    debugServer.stop()
